b29b11635f0674f8e1b98739d529da28d0d1da71
Rachel Elam <relam24@gmail.com>
2020-08-16 14:48:00 -0400

    updates

--- danbraidrachjames/js/app.js
+++ danbraidrachjames/js/app.js
@@ -1,7 +1,17 @@
-var http = require('http');
 
-var server = http.createServer(function (req, res) {
+// Sets package requirements
+var express = require('express')
+var app = express()
+var path = require('path')
+var bodyParser = require('body-parser');
+
+// Setting port to 5000 for heroku
+var PORT = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/public'));
+
+require('./routes/index.js')(app);
+
+app.listen(PORT, function(){
+	console.log("Server listening on: " + PORT);
 });
-
-server.listen(5000);
-console.log('Node.js web server at port 5000 is running..')
